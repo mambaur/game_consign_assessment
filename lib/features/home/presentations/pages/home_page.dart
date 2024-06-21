e76b7e4c9b2d21edfe2cd2588e7749c7ec87b7e9
Mambaur Roziq Alwi <alwiroziq1998@gmail.com>
2024-06-21 15:20:03 +0700

    chore: add workmanager

--- lib/features/home/presentations/pages/home_page.dart
+++ lib/features/home/presentations/pages/home_page.dart
@@ -1,17 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:game_consign_assessment/core/local_notification.dart';
+import 'package:game_consign_assessment/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workmanager/workmanager.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -19,70 +16,122 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int? data = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  getData() async {
+    String prefKey = 'test_data';
+    final prefs = await SharedPreferences.getInstance();
+    int? testData = prefs.getInt(prefKey);
+    print('Test data: $testData');
+    if (mounted) {
+      setState(() {
+        data = testData;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Test Data',
             ),
             Text(
-              '$_counter',
+              data.toString(),
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => getData(),
+            tooltip: 'Refresh',
+            child: const Icon(Icons.refresh),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              localNotification.show(
+                Random().nextInt(10000),
+                'Notifikasi Alert Masuk',
+                "Ini Notifikasi",
+                NotificationDetails(
+                  android: AndroidNotificationDetails(
+                    channel?.id ?? '',
+                    channel?.name ?? '',
+                    channelDescription: channel?.description ?? '',
+                    playSound: true,
+                    // color: Colors.white,
+                    // icon: notificationIcon,
+                  ),
+                ),
+                // payload: payload
+              );
+            },
+            tooltip: 'Notification',
+            child: const Icon(Icons.notifications_outlined),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              prefs.remove('test_data');
+
+              Workmanager().cancelByUniqueName("task-identifier2");
+            },
+            tooltip: 'Remove',
+            child: const Icon(Icons.delete_outline),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              prefs.remove('test_data');
+
+              Workmanager().cancelAll();
+            },
+            tooltip: 'Remove All',
+            child: const Icon(Icons.delete_forever),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () async {
+              Workmanager().registerOneOffTask(
+                "task-one-time",
+                "taskOneTime",
+                initialDelay: const Duration(seconds: 10),
+              );
+            },
+            tooltip: 'Add',
+            child: const Icon(Icons.add),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:game_consign_assessment/core/bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workmanager/workmanager.dart';
 
 import 'app.dart';
 import 'core/local_notification.dart';
@@ -9,11 +12,54 @@ LocalNotification localNotification = LocalNotification();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   // Local Notification Configuration
   await localNotification.init();
 
-  runApp(MultiBlocProvider(
-    providers: BlocSetting.providers(),
-    child: const App(),
-  ));
+  await Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
+
+  Workmanager().registerPeriodicTask(
+    "task-periodic",
+    "taskPeriodic",
+    initialDelay: const Duration(seconds: 5),
+    frequency: const Duration(minutes: 1),
+  );
+
+  runApp(const App());
+
+  // runApp(MultiBlocProvider(
+  //   providers: BlocSetting.providers(),
+  //   child: const App(),
+  // ));
+}
+
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    String prefKey = 'test_data';
+    final prefs = await SharedPreferences.getInstance();
+    int? testData = prefs.getInt(prefKey);
+    int data = (testData ?? 0) + 1;
+    prefs.setInt(prefKey, data);
+
+    LocalNotification localNotification = LocalNotification();
+    await localNotification.init();
+    localNotification.show(
+      Random().nextInt(10000),
+      'Notifikasi Alert Masuk',
+      "Ini Notifikasi $task",
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel?.id ?? '',
+          channel?.name ?? '',
+          channelDescription: channel?.description ?? '',
+          playSound: true,
+          // color: Colors.white,
+          // icon: notificationIcon,
+        ),
+      ),
+    );
+
+    return Future.value(true);
+  });
 }
